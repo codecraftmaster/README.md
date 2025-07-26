c37ae4ebabb4596946963b3c26d5fd3e391f2489
Endluri Indupriya <155086680+codecraftmaster@users.noreply.github.com>
2025-07-26 13:04:21 +0530

    Delete Employee_crud_operation directory

--- Employee_crud_operation/src/main/java/jsp/spring/Employee_crud_operation/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package jsp.spring.Employee_crud_operation;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-
-}
