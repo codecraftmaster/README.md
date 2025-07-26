1a59aca4e949ca75951b93e73a918e56bc0eea06
Endluri Indupriya <155086680+codecraftmaster@users.noreply.github.com>
2025-07-26 13:11:57 +0530

    Delete taskproject directory

--- taskproject/src/main/java/com/teluguSkillHub/taskproject/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.teluguSkillHub.taskproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.teluguSkillHub.taskproject.entity.Users;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-
-}
--- taskproject/src/main/java/com/teluguSkillHub/taskproject/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.teluguSkillHub.taskproject.service;
-
-import com.teluguSkillHub.taskproject.payload.UserDto;
-
-public interface UserService {
-	public UserDto createUser(UserDto userDto);
-	
-
-}
