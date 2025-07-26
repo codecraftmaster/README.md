abf5d0a9b04f018e4d973e398f4c903a1df62759
Endluri Indupriya <155086680+codecraftmaster@users.noreply.github.com>
2025-07-26 13:04:56 +0530

    Delete spring_boot_crud_operation directory

--- spring_boot_crud_operation/src/main/java/jsp/spring/spring_boot_crud_operation/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package jsp.spring.spring_boot_crud_operation;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-
-}
